ab0b06bfb091c87369b1a540ff812eabe3369470
Mario2044 <116796406+Mario2044@users.noreply.github.com>
2023-02-15 15:00:13 +0200

    Add html, js and readme file

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+function sayHello(name) {
+    for (let i = 0; i < name,length; i++) {
+        if (name[i] === '') {
+            return 'Hello, ' + userName + '!';
+        } 
+        else if (name[i] > 4)  {
+            return 'Hello guys, ' + userName + '!';
+        }
+    }    
+}
+
+seyHello("petro");
+seyHello('Petro', 'Ivan', 'Olga');
