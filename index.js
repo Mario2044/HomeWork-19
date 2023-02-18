b0048419eb9239a4ce411cc1f8113ade5f2657cb
Mario2044 <116796406+Mario2044@users.noreply.github.com>
2023-02-18 11:56:08 +0200

    Update code

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
+function sayHello(...names) {
+    if (names.length > 1) {     
+        console.log(`Hello guys, ${names.join(", ")}!`);
+    } else {
+        console.log(`Hello ${names[0]}!`);
+    }
+  }
 
-
-function sayHello(name) {
-    for (let i = 0; i < name,length; i++) {
-        if (name[i] === '') {
-            return 'Hello, ' + userName + '!';
-        } 
-        else if (name[i] > 4)  {
-            return 'Hello guys, ' + userName + '!';
-        }
-    }    
-}
-
-seyHello("petro");
-seyHello('Petro', 'Ivan', 'Olga');
+sayHello("Petro");
+sayHello("Petro", "Ivan", "Olga");
